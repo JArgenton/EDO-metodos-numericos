1ebc3455c4a174b3c00d6d1a630de582a9d83e8e
argenton <joaopedroargenton@hotmail.com>
2024-05-20 17:21:18 -0300

    metodos numericos

---
+++ Euler_melhorado.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import funçao as f
+
+# Yk+1 = Yk + h/2(y'(x, y) + y'(x, y*))
+#y'k = -y + t +1 
+#y* = Yk + hy'
+
+# para melhor entedimento, usarei t no lugar de x, representando a funçao no tempo. 
+
+
+
+def Euler_Melhorado(coord_inicial: 'tuple[float, float]', intervalo: 'tuple[float, float]', n_passos):
+
+    passo = (intervalo[1] - intervalo[0]) / n_passos
+    x_values = [coord_inicial[0]]
+    y_values = [coord_inicial[1]]
+
+    for _ in range(n_passos):
+        x = x_values[-1]
+        y = y_values[-1]
+
+        k1 = f.derivada_funçao(x, y)
+        print(f'K1 melhorado = {k1}')
+        k2 = f.derivada_funçao(x + passo, y + passo * k1)
+        print(f'K2 melhorado = {k2}')
+        newy = y + (passo / 2) * (k1 + k2)
+        print(f'newy melhorado = {newy}')
+
+        x_values.append(x + passo)
+        y_values.append(newy)
+
+    return x_values, y_values
+
+    
+
+
+        
---
+++ funçao.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def derivada_funçao(t, y):
+    #isolando dy/dx temos: y' = -y + t + 1 
+
+    y_linha =  -y + t + 1 
+    return y_linha 
+
