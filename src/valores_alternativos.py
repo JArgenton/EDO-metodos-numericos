68f24f73d80d8322c3d407b6ab87e4dc0bbe2100
Fackjoao <joaopedroargenton@hotmail.com>
2024-05-21 16:29:31 -0300

    sla cara, veja valores alternativos ai

--- src/valores_alternativos.py
+++ src/valores_alternativos.py
@@ -24,9 +24,26 @@ def achavalores(F: Fx, x: float, y: float, a, b, p, q):
     """funcao qe faz coisas"""
     h = F.h
     K1 = F.derivada(x, y)
-    K2 = F.derivada(x + p*h, y + q*h*F.derivada(x, y))
+    K2 = F.derivada(x + p * h, y + q * h * F.derivada(x, y))
     dy = (a*K1 + b*K2)
     return dy
+
+
+def faz_coisas(a: float, F: Fx, df: pd.DataFrame):
+    """"teste"""
+    b = 1 - a
+    q = 0.5/b
+    print(q)
+    p = q
+    x = [F.inicial[0]]
+    y = [F.inicial[1]]
+    for _ in range(F.n_passos):
+        newx = x[-1] + F.h
+        newy = achavalores(F, x[-1], y[-1], a, b, p, q)
+
+        x.append(newx)
+        y.append(newy)
+    monta_database(df, a, y)
 
 
 def make_achavalores(F: Fx):
@@ -34,21 +51,7 @@ def make_achavalores(F: Fx):
     data = {}
     df = pd.DataFrame(data)
     a = 0
-    while a <= 1:
-
-        b = 1 - a
-        q = 0.5/b
-        p = q
-
-        x = [F.inicial[0]]
-        y = [F.inicial[1]]
-        for _ in range(F.n_passos):
-            newx = x[-1] + F.h
-            newy = achavalores(F, x[-1], y[-1], a, b, p, q)
-            x.append(newx)
-            y.append(newy)
-        monta_database(df, a, y)
-        x.clear()
-        y.clear()
-        a = a + 0.1
+    while a < 0.9:
+        faz_coisas(a, F, df)
+        a = a + 0.3
     export_to_excel(df, "melhor valor.xlsx")
